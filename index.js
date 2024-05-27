faf06077fcb309849e98d42158b1819642338445
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-05-27 10:08:28 +0530

    comments

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ require("dotenv").config;
 
 const PORT = process.env.PORT || 5000;
 
-const connection = mongoose.connect(process.env.MONGO_URL || "mongodb+srv://21955a1206:nikhil1528@project.cyg4qu7.mongodb.net/Travel?retryWrites=true&w=majority&appName=Project")
-.then(() => console.log("Connected"))
-.catch((err) => console.log(err));
+// const connection = mongoose.connect(process.env.MONGO_URL || "mongodb+srv://21955a1206:nikhil1528@project.cyg4qu7.mongodb.net/Travel?retryWrites=true&w=majority&appName=Project")
+// .then(() => console.log("Connected"))
+// .catch((err) => console.log(err));
 
 const app = express();
 
@@ -18,6 +18,6 @@ app.listen(PORT, ()=>{
     console.log(`Running in ${PORT}`)
 })
 
-app.get('', (req,res) => {
+app.get('/', (req,res) => {
     res.json("WELCOME");
 })
