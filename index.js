580dabcb7b364c83b44ef17d7247f7b7ea01ed4a
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-05-27 09:13:02 +0530

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
