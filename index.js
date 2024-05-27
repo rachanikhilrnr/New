a98b73da6d75652089fc6181aca47ff791362b67
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-05-27 09:53:08 +0530

    second

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
+require("dotenv").config;
 
+const PORT = process.env.PORT || 5000;
+
+const connection = mongoose.connect(process.env.MONGO_URL || "mongodb+srv://21955a1206:nikhil1528@project.cyg4qu7.mongodb.net/Travel?retryWrites=true&w=majority&appName=Project")
+.then(() => console.log("Connected"))
+.catch((err) => console.log(err));
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.listen(PORT, ()=>{
+    console.log(`Running in ${PORT}`)
+})
+
+app.get('', (req,res) => {
+    res.json("WELCOME");
+})
